1cb7345aeb74e780b2818c07b828cf5c2a0c7e34
HananhHamed <36267684+HananhHamed@users.noreply.github.com>
2018-02-12 15:16:33 +0100

    ATM Class
    Udacity Project ----> task no.10

--- atm.py
+++ atm.py
@@ -1,22 +1,43 @@
-moneyList = [100 , 50 , 10 , 5]
-
-def printData(money , count):
-    while(count > 0):
-        print "give "+ str(money)
-        count=count-1
-
-def ATM (money):
-    i=0
-    while( i < len(moneyList) ):
-        if ( money >= moneyList[i] ):
-            x = int(money / moneyList[i])
-            printData ( moneyList[i] , x )
-            money = money - x * moneyList[i]
-            if ( money < moneyList[-1] ):
-                print "give ", money
-                break
-            i = i + 1
+class ATM:
+    def __init__(self, balance , bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
+    moneyList = [100 , 50 , 10 , 5]
+    #create printdata function to help withdraw function
+    def printData(self , money , count):
+        while(count > 0):
+            print "give " + str(money)
+            count = count-1
+    #create withdraw function
+    def withdraw (self , request):
+        print "Welcome to " + self.bank_name
+        print "Current Balance = " + str(self.balance)
+        print "========================="
+        if ( request < self.balance ):
+            self.balance = self.balance - request
+            i=0
+            while( i < len( self.moneyList ) ):
+                if ( request >= self.moneyList[i] ):
+                    x = int(request / self.moneyList[i])
+                    self.printData ( self.moneyList[i] , x )
+                    request = request - x * self.moneyList[i]
+                    if ( request < self.moneyList[-1] ):
+                        print "give ", request
+                        break
+                    i = i + 1
+                else:
+                    i = i + 1
         else:
-            i = i + 1
+            print "Can't give you all this money !!"
 
-ATM(273)
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
