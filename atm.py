b977e5e8a61889924db366a50dcc0b1d85c0c959
HananhHamed <36267684+HananhHamed@users.noreply.github.com>
2018-02-12 17:17:06 +0100

    Updated ATM class with show withdrawals function
    Udacity projects --->  updated task11

--- atm.py
+++ atm.py
@@ -3,27 +3,29 @@ class ATM:
         self.balance = balance
         self.bank_name = bank_name
         self.withdrawal_list = []
-    moneyList = [100 , 50 , 10 , 5]
-    #create printdata function to help withdraw function
+
+    '''create printdata function to help withdraw function'''
     def printData(self , money , count):
-        while(count > 0):
+        while count > 0:
             print "give " + str(money)
-            count = count-1
-    #create withdraw function
+            count = count - 1
+
+    '''create withdraw function'''
     def withdraw (self , request):
         print "Welcome to " + self.bank_name
         print "Current Balance = " + str(self.balance)
         print "========================="
-        if ( request < self.balance ):
+        if request < self.balance:
+            moneyList = [100 , 50 , 10 , 5]
             self.balance = self.balance - request
             self.withdrawal_list.append( request )
             i=0
-            while( i < len( self.moneyList ) ):
-                if ( request >= self.moneyList[i] ):
-                    x = int(request / self.moneyList[i])
-                    self.printData ( self.moneyList[i] , x )
-                    request = request - x * self.moneyList[i]
-                    if ( request < self.moneyList[-1] ):
+            while i < len(moneyList):
+                if request >= moneyList[i]:
+                    x = int(request / moneyList[i])
+                    self.printData(moneyList[i] , x)
+                    request = request - x * moneyList[i]
+                    if request < moneyList[-1]:
                         print "give ", request
                         break
                     i = i + 1
@@ -31,11 +33,12 @@ class ATM:
                     i = i + 1
         else:
             print "Can't give you all this money !!"
-    #create show_withdrawals function
-    def show_withdrawals( self ):
+
+    '''create show_withdrawals function'''
+    def show_withdrawals(self):
         print "Your Last Withdrawals: "
         for withdrawal in self.withdrawal_list:
-             print withdrawal 
+             print withdrawal
 
 balance1 = 500
 balance2 = 1000
